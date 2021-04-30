3b4d784413006dc902ff7021ea057c1bcf00c9e1
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-30 19:02:47 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCICE 07 - ARRAY M06/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace EXERCICE_07___ARRAY_M06 {
+    //-----> INICIO
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            Student[] vect = new Student[10]; //-------> VETOR DE CLASSE
+
+            Console.WriteLine("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i=1; i <=n; i++) {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.WriteLine("Quarto: ");
+                int room = int.Parse(Console.ReadLine());
+                vect[room] = new Student(name, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for(int i =0; i < 10; i++) { 
+                if (vect[i] != null) {
+                    Console.WriteLine(i + ": "+ vect[i]);
+                }
+            }
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> FIM
+}
---
+++ EXERCICE 07 - ARRAY M06/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCICE_07___ARRAY_M06 {
+    
+    class Student {
+        //---------------------------- START --------------------------------//
+        public string Name { get; set; }
+        public string Email { get; set; }
+        //------------------------- CONSTRUCTORS ----------------------------//
+        public Student(string name, string email) {
+            Name = name;
+            Email = email;
+        }
+        //------------------------- CLASS |  MET ---------------------------//
+        public override string ToString() {
+            return Name + ", " + Email;
+        }
+        //-------------------------------- END -------------------------------//
+    }
+
+}
